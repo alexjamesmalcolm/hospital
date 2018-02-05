f862b69bd506c8e1654db8a124c802666443ec74
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-05 17:02:17 -0500

    SurgeonTest: shouldHaveSalary

--- src/main/java/hospital/Surgeon.java
+++ src/main/java/hospital/Surgeon.java
@@ -1,11 +1,16 @@
 package hospital;
 
 public class Surgeon extends Doctor {
-	
+
 	private boolean isOperating;
 
 	public Surgeon(String name, int employeeNumber, String specialty, boolean isOperating) {
 		super(name, employeeNumber, specialty);
 		this.isOperating = isOperating;
+	}
+
+	@Override
+	public int getSalary() {
+		return 120000;
 	}
 }
--- src/test/java/hospital/SurgeonTest.java
+++ src/test/java/hospital/SurgeonTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 public class SurgeonTest {
-	
+
 	@Test
 	public void shouldHaveName() {
 		String name = "Phil";
@@ -16,5 +16,19 @@ public class SurgeonTest {
 		Surgeon underTest = new Surgeon(name, employeeNumber, specialty, isOperating);
 		String returnedName = underTest.getName();
 		assertThat(returnedName, is("Phil"));
+	}
+
+	@Test
+	public void shouldHaveEmployeeNumber() {
+		Surgeon underTest = new Surgeon("", 111, "", true);
+		int number = underTest.getEmployeeNumber();
+		assertThat(number, is(111));
+	}
+
+	@Test
+	public void shouldHaveSalary() {
+		Surgeon underTest = new Surgeon(null, 0, null, false);
+		int salary = underTest.getSalary();
+		assertThat(salary, is(120000));
 	}
 }
